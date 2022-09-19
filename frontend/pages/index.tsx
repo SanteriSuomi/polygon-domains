7fd5dca323318409b016b885da6c3dcbde43210f
Santeri Suomi <sant.suomi@gmail.com>
2022-09-19 21:15:32 +0300

    Implement domain minting through the frontend

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,27 +2,68 @@ import type { NextPage } from "next";
 import { useEffect, useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
+import { ethers } from "ethers";
+import { BigNumber } from "ethers/lib/ethers";
 
 import styles from "../styles/index.module.css";
 import logo from "../public/images/polygon_logo.svg";
 import animation from "../public/images/polygon_rotating.gif";
 import loading from "../public/images/loading.gif";
 
+import contractJson from "../public/abi/Domains.json";
+
 interface HomeState {
-	account: any;
+	account?: any;
 	connectText: string;
+	contract?: ethers.Contract;
 }
+
+const CONTRACT_ADDRESS = "0xDD07A3dDA0664804A81Ce8b2D38E6157c477Ed5a";
 
 const Home: NextPage = () => {
 	const [data, setData] = useState<HomeState>({
-		account: undefined,
 		connectText: "No wallet found",
 	});
 	const [connecting, setConnecting] = useState(false);
+	const [domainName, setDomainName] = useState("");
+	const [domainPrice, setDomainPrice] = useState("");
+	const [domainData, setDomainData] = useState("");
+
+	const mintDomain = async () => {
+		if (!domainName || domainName.length < 1) {
+			alert("No domain name selected");
+			return;
+		}
+		const { ethereum } = window as any;
+		if (ethereum) {
+			const txn: ethers.ContractTransaction =
+				await data.contract?.register(domainName, domainData, {
+					value: domainPrice,
+				});
+			const receipt = await txn.wait();
+			if (receipt.status === 1) {
+				alert(
+					"Domain minted! Transaction: https://mumbai.polygonscan.com/tx/" +
+						txn.hash
+				);
+			} else {
+				alert("Something went wrong with the transaction");
+			}
+		}
+	};
+
+	const getDomainPrice = async (domainName: string): Promise<BigNumber> => {
+		return data.contract?.getPrice(domainName);
+	};
+
+	const createContractObject = (ethereum: any) => {
+		const provider = new ethers.providers.Web3Provider(ethereum);
+		const signer = provider.getSigner();
+		return new ethers.Contract(CONTRACT_ADDRESS, contractJson.abi, signer);
+	};
 
 	const connectWallet = async () => {
 		const { ethereum } = window as any;
-
 		try {
 			setConnecting(true);
 			const accounts = await ethereum.request({
@@ -32,6 +73,7 @@ const Home: NextPage = () => {
 			setData({
 				...data,
 				account: accounts[0],
+				contract: createContractObject(ethereum),
 			});
 		} catch (error: any) {
 			setConnecting(false);
@@ -41,21 +83,20 @@ const Home: NextPage = () => {
 
 	const checkWalletConnection = async () => {
 		const { ethereum } = window as any;
-
 		if (ethereum) {
 			console.log("MetaMask found");
 		} else {
-			console.log("No MetaMask found");
+			console.warn("No MetaMask found");
 			return;
 		}
 
 		const accounts = await ethereum.request({ method: "eth_accounts" });
-
 		if (accounts.length !== 0) {
 			console.log("Authorized account found");
 			setData({
 				...data,
 				account: accounts[0],
+				contract: createContractObject(ethereum),
 			});
 		} else {
 			console.log("No authorized accounts found");
@@ -73,8 +114,48 @@ const Home: NextPage = () => {
 	const renderContent = () => {
 		if (data.account) {
 			return (
-				<div className={styles.walletconnected}>
-					Wallet connected! Hooray!
+				<div className={styles.walletconnectedcontent}>
+					<p className={styles.minttitle}>Mint a Domain</p>
+					<div className={styles.mintform}>
+						<div className={styles.mintdomaininputwrapper}>
+							<input
+								className={styles.mintdomaininput}
+								placeholder="domain name"
+								onChange={async (event) => {
+									const value = event.currentTarget.value;
+									if (!value) {
+										setDomainPrice("");
+										return;
+									}
+									setDomainPrice(
+										(await getDomainPrice(value)).toString()
+									);
+									setDomainName(value);
+								}}
+							></input>
+							<div className={styles.mintdomaintld}>.matic</div>
+							<div className={styles.mintdomainprice}>
+								{domainPrice.length > 0
+									? `${ethers.utils.formatEther(
+											domainPrice
+									  )} matic`
+									: ""}
+							</div>
+						</div>
+						<input
+							className={styles.mintdatainput}
+							placeholder="domain data (optional)"
+							onChange={(event) => {
+								setDomainData(event.currentTarget.value);
+							}}
+						></input>
+						<button
+							className={styles.mintsubmit}
+							onClick={mintDomain}
+						>
+							Mint
+						</button>
+					</div>
 				</div>
 			);
 		}
