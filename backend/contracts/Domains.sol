b2c40aa369cd62ba9afb124a449c677cd48818ba
Santeri Suomi <sant.suomi@gmail.com>
2022-10-12 17:29:45 +0300

    Add methods to get domain lease price and to renew a lease

--- backend/contracts/Domains.sol
+++ backend/contracts/Domains.sol
@@ -34,15 +34,18 @@ contract Domains is ERC721URIStorage, Ownable {
     uint256 public minDomainLength = 0;
     // Price units, the number zeroes it has (wei), more zeroes = bigger price
     uint256 public priceUnits = 17;
+    uint256 public maxLeaseTime = 365 days;
 
     string private svgStart =
         '<svg xmlns="http://www.w3.org/2000/svg" width="270" height="270" fill="none"><path fill="url(#B)" d="M0 0h270v270H0z"/><defs><filter id="A" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse" height="270" width="270"><feDropShadow dx="0" dy="1" stdDeviation="2" flood-opacity=".225" width="200%" height="200%"/></filter></defs><path d="M72.863 42.949c-.668-.387-1.426-.59-2.197-.59s-1.529.204-2.197.59l-10.081 6.032-6.85 3.934-10.081 6.032c-.668.387-1.426.59-2.197.59s-1.529-.204-2.197-.59l-8.013-4.721a4.52 4.52 0 0 1-1.589-1.616c-.384-.665-.594-1.418-.608-2.187v-9.31c-.013-.775.185-1.538.572-2.208a4.25 4.25 0 0 1 1.625-1.595l7.884-4.59c.668-.387 1.426-.59 2.197-.59s1.529.204 2.197.59l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616c.384.665.594 1.418.608 2.187v6.032l6.85-4.065v-6.032c.013-.775-.185-1.538-.572-2.208a4.25 4.25 0 0 0-1.625-1.595L41.456 24.59c-.668-.387-1.426-.59-2.197-.59s-1.529.204-2.197.59l-14.864 8.655a4.25 4.25 0 0 0-1.625 1.595c-.387.67-.585 1.434-.572 2.208v17.441c-.013.775.185 1.538.572 2.208a4.25 4.25 0 0 0 1.625 1.595l14.864 8.655c.668.387 1.426.59 2.197.59s1.529-.204 2.197-.59l10.081-5.901 6.85-4.065 10.081-5.901c.668-.387 1.426-.59 2.197-.59s1.529.204 2.197.59l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616c.384.665.594 1.418.608 2.187v9.311c.013.775-.185 1.538-.572 2.208a4.25 4.25 0 0 1-1.625 1.595l-7.884 4.721c-.668.387-1.426.59-2.197.59s-1.529-.204-2.197-.59l-7.884-4.59a4.52 4.52 0 0 1-1.589-1.616c-.385-.665-.594-1.418-.608-2.187v-6.032l-6.85 4.065v6.032c-.013.775.185 1.538.572 2.208a4.25 4.25 0 0 0 1.625 1.595l14.864 8.655c.668.387 1.426.59 2.197.59s1.529-.204 2.197-.59l14.864-8.655c.657-.394 1.204-.95 1.589-1.616s.594-1.418.609-2.187V55.538c.013-.775-.185-1.538-.572-2.208a4.25 4.25 0 0 0-1.625-1.595l-14.993-8.786z" fill="#fff"/><defs><linearGradient id="B" x1="0" y1="0" x2="270" y2="270" gradientUnits="userSpaceOnUse"><stop stop-color="#cb5eee"/><stop offset="1" stop-color="#0cd7e4" stop-opacity=".99"/></linearGradient></defs><text x="32.5" y="231" font-size="27" fill="#fff" filter="url(#A)" font-family="Plus Jakarta Sans,DejaVu Sans,Noto Color Emoji,Apple Color Emoji,sans-serif" font-weight="bold">';
     string private svgEnd = "</text></svg>";
     Counters.Counter private _tokenIds;
+    uint256 private leasePriceDecimalAccuracy = 1000;
 
     struct Domain {
         address owner;
         string data;
+        uint256 leaseStartTime;
     }
 
     struct DomainComplete {
@@ -50,6 +53,7 @@ contract Domains is ERC721URIStorage, Ownable {
         address owner;
         string data;
         string uri;
+        uint256 leaseStartTime;
     }
 
     mapping(string => Domain) private domainNameToDomainObject;
@@ -61,11 +65,11 @@ contract Domains is ERC721URIStorage, Ownable {
         external
         payable
     {
-        if (isRegistered(domainName)) revert AlreadyRegistered();
         if (!checkNameValidity(domainName)) revert NotValidName();
-        if (!(msg.value == getPrice(domainName))) revert NotEnoughEtherPaid();
+        if (isRegistered(domainName)) revert AlreadyRegistered();
+        if (msg.value != getPrice(domainName)) revert NotEnoughEtherPaid();
 
-        Domain memory domain = Domain(msg.sender, data);
+        Domain memory domain = Domain(msg.sender, data, block.timestamp);
         domainNameToDomainObject[domainName] = domain;
         uint256 tokenId = _tokenIds.current();
         tokenIdToDomain[tokenId] = domainName;
@@ -75,6 +79,15 @@ contract Domains is ERC721URIStorage, Ownable {
         string memory svg = string.concat(svgStart, name, svgEnd);
         _setTokenURI(tokenId, getJsonUri(name, svg));
         emit Registered(msg.sender, domainName, msg.sender, domainName);
+    }
+
+    function renewLease(string calldata domainName) external payable {
+        if (!checkNameValidity(domainName)) revert NotValidName();
+        if (!isRegistered(domainName)) revert NotRegistered();
+        if (msg.value != getLeaseRenewCost(domainName))
+            revert NotEnoughEtherPaid();
+
+        domainNameToDomainObject[domainName].leaseStartTime = block.timestamp;
     }
 
     function modifyData(string calldata domainName, string calldata data)
@@ -127,7 +140,8 @@ contract Domains is ERC721URIStorage, Ownable {
                     domainName,
                     domain.owner,
                     domain.data,
-                    tokenURI(i)
+                    tokenURI(i),
+                    domain.leaseStartTime
                 );
             }
         }
@@ -146,6 +160,34 @@ contract Domains is ERC721URIStorage, Ownable {
             paymentVar = domainLengthPriceUpperBound;
         }
         return (domainLengthPriceUpperBound - paymentVar) * 10**priceUnits;
+    }
+
+    function getLeaseTimeRemaining(string calldata domainName)
+        public
+        view
+        returns (uint256)
+    {
+        return
+            block.timestamp -
+            domainNameToDomainObject[domainName].leaseStartTime;
+    }
+
+    function getLeaseRenewCost(string calldata domainName)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 leaseTimeUsed = maxLeaseTime -
+            getLeaseTimeRemaining(domainName);
+        // Scale lease time to a number between 1 and leasePriceDecimal accuracy for new lease cost
+        // Cost is proportional to the current lease time used - the less lease time has been used, the less
+        // renewal of lease will cost
+        uint256 leaseTimeUsedScaled = (leasePriceDecimalAccuracy - 1) *
+            ((leaseTimeUsed - 1) / (maxLeaseTime - 1)) +
+            1;
+        uint256 leaseRenewCost = (getPrice(domainName) * leaseTimeUsedScaled) /
+            maxLeaseTime;
+        return leaseRenewCost;
     }
 
     function _afterTokenTransfer(
