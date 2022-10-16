6e37d9919d7e5b890c1a944a0eef95647168428f
SanteriSuomi <sant.suomi@gmail.com>
2022-10-16 23:59:42 +0300

    Remove console import

--- backend/contracts/Domains.sol
+++ backend/contracts/Domains.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "hardhat/console.sol";
 
 import {StringUtils} from "./libraries/StringUtils.sol";
 
